8865ec329acfd6c6005a9451ba9d41711995ca3a
Maidul Hasan <hmd87898@gmail.com>
2025-05-22 20:35:21 +0600

    Add project files.

---
+++ ProductCategoryAPI/Models/Category.cs
@@ -0,0 +1,12 @@
+﻿namespace ProductCategoryAPI.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        // Navigation
+        public ICollection<Product>? Products { get; set; }
+    }
+
+}
---
+++ ProductCategoryAPI/Models/ProductCategoryDbContext.cs
@@ -0,0 +1,14 @@
+﻿namespace ProductCategoryAPI.Models
+{
+    using Microsoft.EntityFrameworkCore;
+    using ProductCategoryAPI.Models;
+
+    public class ProductCategoryDbContext : DbContext
+    {
+        public ProductCategoryDbContext(DbContextOptions<ProductCategoryDbContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+
+}
